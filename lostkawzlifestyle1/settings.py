adc287ca6667b285f1add4a1f2aea0413a74b63b
Carlos Ruiz <losman333@hotmail.com>
2017-04-21 08:32:20 -0700

    hereagain

--- lostkawzlifestyle1/settings.py
+++ lostkawzlifestyle1/settings.py
@@ -227,3 +227,5 @@ STATIC_URL = '/static/'
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES = { 'default': dj_database_url.config() }
