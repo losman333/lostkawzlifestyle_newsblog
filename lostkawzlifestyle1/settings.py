db3dc79cb047bdfa95e422a2c9af14b8a06621d9
Carlos Ruiz <losman333@hotmail.com>
2017-04-21 08:07:46 -0700

    the db

--- lostkawzlifestyle1/settings.py
+++ lostkawzlifestyle1/settings.py
@@ -147,7 +147,16 @@ WSGI_APPLICATION = 'lostkawzlifestyle1.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lostkawzlifestyle',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '8080',
+        'PASSWORD': 'batman123',
+    }
+}
 
 
 # Password validation
