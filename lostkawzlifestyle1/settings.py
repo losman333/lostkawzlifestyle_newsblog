aa477a4121a259d8c612b50b2b20a6cb59d61455
Carlos Ruiz <losman333@hotmail.com>
2017-05-01 09:30:57 -0700

    hereweare

--- lostkawzlifestyle1/settings.py
+++ lostkawzlifestyle1/settings.py
@@ -240,8 +240,6 @@ STATICFILES_DIRS = [
 ]
 
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
