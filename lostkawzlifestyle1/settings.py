590b00f7ca5c0fe7cca05fed27b9af144b421a09
Carlos Ruiz <losman333@hotmail.com>
2017-05-07 07:18:19 -0700

    yah

--- lostkawzlifestyle1/settings.py
+++ lostkawzlifestyle1/settings.py
@@ -223,7 +223,7 @@ DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 STATIC_ROOT = 'lostkawzlifestyle1/staticfiles/'
 
 STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATICFILES_STORAGE = ''whitenoise.django.GzipManifestStaticFilesStorage''
 
 STATIC_URL = "https://%s/%s/" % (AWS_CLOUDFRONT_DOMAIN, STATICFILES_LOCATION)
 
