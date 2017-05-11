5b21fff8619b5b89afb668b7f4d380e24820a6c9
Carlos Ruiz <losman333@hotmail.com>
2017-05-11 14:51:45 -0700

    this

--- lostkawzlifestyle1/settings.py
+++ lostkawzlifestyle1/settings.py
@@ -208,7 +208,7 @@ AWS_STORAGE_BUCKET_NAME = config('BUCKET_NAME')
 AWS_CLOUDFRONT_DOMAIN = config('CD_DOMAIN')
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID') 
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-    
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME   
 AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'Cache-Control': 'max-age=94608000',
