253cab451e9e8564fb07e76074ac977436c7f24d
Carlos Ruiz <losman333@hotmail.com>
2017-05-13 07:32:28 -0700

    hes

--- lostkawzlifestyle1/settings.py
+++ lostkawzlifestyle1/settings.py
@@ -211,7 +211,7 @@ AWS_S3_ACCESS_KEY_ID = config('AWS_S3_ACCESS_KEY_ID')
 AWS_S3_SECRET_ACCESS_KEY = config('AWS_S3_SECRET_ACCESS_KEY')
 AWS_S3_SECURE_URLS = False       # use http instead of https
 AWS_QUERYSTRING_AUTH = False
-AWS_S3_HOST = "s3-eu-west-1.amazonaws.com"  # Change to the media center you chose when creating the bucket
+AWS_S3_HOST = "s3-us-west-1.amazonaws.com"  # Change to the media center you chose when creating the bucket
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME   
 AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
