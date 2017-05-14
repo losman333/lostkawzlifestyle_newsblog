d87782836b9a0ce4cc726f93dfb048386efd84d5
Carlos Ruiz <losman333@hotmail.com>
2017-05-14 08:41:02 -0700

    this

--- lostkawzlifestyle1/settings.py
+++ lostkawzlifestyle1/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
-import os
 from decouple import config, Csv
+import dj_database_url
+import os
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -244,6 +246,5 @@ STATICFILES_DIRS = [
 ]
 
 
-import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
