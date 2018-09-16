6259b0b08a8035d5b22d2f02d6e6cbc485174fef
YOUR_USERNAME <losman3331984@gmail.com>
2018-09-16 09:10:58 -0700

    addde

--- lostkawzlifestyle1/settings.py
+++ lostkawzlifestyle1/settings.py
@@ -227,14 +227,14 @@ DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 STATIC_ROOT = 'lostkawzlifestyle1/staticfiles/'
 
 STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+STATIC_URL = '/static/'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+##STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static"),]
 
 
 db_from_env = dj_database_url.config(conn_max_age=500)
